e077ea1f78d7ec2797021775b37013bb30a51952
gicunho <gicunho@hotmail.it>
2021-04-28 17:29:59 +0200

    Aggiunto Lista HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,9 +9,12 @@ var cognomiNew = cognomi.sort();
 console.log(cognomiNew);
 //4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
 var lista = '';
+var listaUl = '';
 
 for (var i = 0; i < cognomiNew.length; i++) {
     //console.log(cognomiNew[i]);
     lista =(i + 1) +') ' + cognomiNew[i];
     console.log(lista);
+    listaUl = document.getElementById('cognomi').innerHTML;
+    document.getElementById('cognomi').innerHTML = document.getElementById('cognomi').innerHTML + '<li>' + lista + '</li>';    
 }
